639c947dd774af007a08d5853b1f7992e137e6b2
MaxOF <crokster1996@gmail.com>
2022-07-08 14:37:50 +0300

    install basic libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import {Header} from "./components/pages/templates/organisms/header/Header";
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+export const App = () => {
+    return (
+        <div className="App">
+            <Header/>
+        </div>
+    );
 }
-
-export default App;
