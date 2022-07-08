569e747d6958d1c87a60ad9f0dd0d4af20c90a06
MaxOF <crokster1996@gmail.com>
2022-07-08 23:48:39 +0300

    add new block services with styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './App.module.scss';
 import {Header} from "./components/pages/templates/organisms/header/Header";
 import {Main} from "./components/pages/templates/organisms/main/Main";
+import {Services} from "./components/pages/templates/organisms/services/Services";
 
 export const App = () => {
     return (
@@ -9,6 +10,7 @@ export const App = () => {
             <Header/>
             <main className={styles.page}>
                 <Main />
+                <Services />
             </main>
         </div>
     );
