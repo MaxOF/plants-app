392b392ebe30fb665960567c52a11251ce423a17
MaxOF <crokster1996@gmail.com>
2022-07-10 21:55:48 +0300

    add new block with cards

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import styles from './App.module.scss';
 import {Header} from "./components/pages/templates/organisms/header/Header";
 import {Main} from "./components/pages/templates/organisms/main/Main";
 import {Services} from "./components/pages/templates/organisms/services/Services";
+import {Cards} from "./components/pages/templates/organisms/cards/Cards";
 
 export const App = () => {
     return (
@@ -11,6 +12,7 @@ export const App = () => {
             <main className={styles.page}>
                 <Main />
                 <Services />
+                <Cards />
             </main>
         </div>
     );
